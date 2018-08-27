b33b1cdba2d9d52e4a00420e5755eeb671b1b158
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-27 20:59:19 +0530

    d to f

---
+++ m1/Degreestofahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Degreestofahrenheit{
+	public static void main(String[]args)
+	{
+		Scanner a=new Scanner(System.in);
+		float d=a.nextInt();
+		float f;
+		f=d*9/5+32;
+		System.out.println(f);
+	}
+}
