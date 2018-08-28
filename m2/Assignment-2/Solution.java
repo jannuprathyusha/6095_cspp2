ccfacbeb5f4f57352bd335b121ce7d0367a4d529
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-28 18:15:33 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -14,10 +14,10 @@ public class Solution {
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
-	public static void rootsOfQuadraticEquation(int a,int b,int c)
+	public static void rootsOfQuadraticEquation(int a, int b, int c)
 	{
-		double d = (b * b) - (4* a *c);
-		double qu=Math.sqrt(d);
+		double d = (b * b) - (4 * a * c);
+		double qu = Math.sqrt(d);
 		double r1 = ((- b + qu) / (2 * a));
 		double r2 = ((- b - qu) / (2 * a));
 		System.out.println(r1 + " " +r2);
