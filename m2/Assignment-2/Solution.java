af9d7800010934b639e678a6c29a2fce6d56e7bb
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-28 18:18:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	public static void rootsOfQuadraticEquation(int a, int b, int c)
-	{
-		double d = (b * b) - (4 * a * c);
-		double qu = Math.sqrt(d);
-		double r1 = ((- b + qu) / (2 * a));
-		double r2 = ((- b - qu) / (2 * a));
-		System.out.println(r1 + " " +r2);
-	}
+    /*
+    Do not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+    public static void rootsOfQuadraticEquation(int a, int b, int c)
+    {
+        double d = (b * b) - (4 * a * c);
+        double qu = Math.sqrt(d);
+        double r1 = (( - b + qu) / (2 * a));
+        double r2 = (( - b - qu) / (2 * a));
+        System.out.println(r1 + " " +r2);
+    }
 }
