77ab8d7d7f49b8229ea4a5d59e300e118b839ddd
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-29 14:33:28 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/**Do not modify this main function.
+*/
+public class Solution {
+	/** The program is to find GCD
+	*/
+	 private Solution() {
+    /** this is a constructor.
+    */
+    }
+    /**
+    Do not modify this main function.
+    *@param args String
+    */
+	public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        gcd(n1,n2);
+        System.out.println(gcd(n1,n2));
+    }
+    /**
+	*Need to write the gcd function and print the output.
+	*@param a int
+    *@param b int
+	*/
+	public static int gcd(int a,int b ){
+		if(a==0 && b!=0){
+			return b;
+		}
+		if(a!=0 && b==0){
+			return a;
+		}
+		if(a==b){
+			return a;
+		}
+		if(a>b){
+			return gcd(a-b,b);
+		}
+		return gcd(a,b-a);
+	}
+}
