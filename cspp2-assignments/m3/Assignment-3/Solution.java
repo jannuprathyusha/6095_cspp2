6c173a99dbdbcf3574fe594158db326ad449c783
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-29 14:41:35 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 /**Do not modify this main function.
 */
 public class Solution {
-	/** The program is to find GCD
-	*/
-	 private Solution() {
+    /** The program is to find GCD.
+    */
+     private Solution() {
     /** this is a constructor.
     */
     }
@@ -12,32 +12,32 @@ public class Solution {
     Do not modify this main function.
     *@param args String
     */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        gcd(n1,n2);
-        System.out.println(gcd(n1,n2));
+        gcd(n1, n2);
+        System.out.println(gcd(n1, n2));
     }
     /**
-	*Need to write the gcd function and print the output.
-	*@param a int
+    *Need to write the gcd function and print the output.
+    *@param a int
     *@param b int
-	*/
-	public static int gcd(final int a, final int b ){
-		if (a == 0 && b != 0) {
-			return b;
-		}
-		if (a != 0 && b == 0) {
-			return a;
-		}
-		if (a == b) {
-			return a;
-		}
-		if (a > b) {
-			return gcd(a - b, b);
-		}
-		return gcd(a, b - a);
-	}
+    */
+    public static int gcd(final int a, final int b ) {
+        if (a == 0 && b != 0) {
+            return b;
+        }
+        if (a != 0 && b == 0) {
+            return a;
+        }
+        if (a == b) {
+            return a;
+        }
+        if (a > b) {
+            return gcd(a - b, b);
+        }
+        return gcd(a, b - a);
+    }
 }
