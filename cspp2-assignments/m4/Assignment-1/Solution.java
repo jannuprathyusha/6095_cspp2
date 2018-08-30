96591826e6d5ab81ac586353316e55a6bee82625
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-30 19:21:01 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -28,7 +28,7 @@ public final class Solution {
         }
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if ( arr[i] > max ) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
