32bb5f2af9e40c16fae992bae33594a86e2a034a
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-30 19:15:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 public final class Solution {
- 	/**
- 	*Fill this main function to print maximum of given array.
-	*/
-	   private Solution() {
-	   	//This sis a constructor.
-	   }
-	   /**
-	   * @param args of the program.
-	   */
-	public static void main( final String[] args ) {
-		/**
-		* main function
-		*/
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		int max = arr[0];
-		for (int i = 0; i < arr.length; i++) {
-			if ( arr[i] > max) {
-				max = arr[i];
-			}
-		}
-		System.out.println(max);
-	}
+    /**
+    *Fill this main function to print maximum of given array.
+    */
+       private Solution() {
+        //This sis a constructor.
+       }
+       /**
+       * @param args of the program.
+       */
+    public static void main (final String[] args) {
+        /**
+        * main function
+        */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if ( arr[i] > max ) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
