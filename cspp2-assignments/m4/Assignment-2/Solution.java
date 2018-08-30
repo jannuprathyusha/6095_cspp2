f27c4a63b21f5d1fb212ed3b4c20cf6c035646bf
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-30 15:48:50 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -31,7 +31,7 @@ public class Solution {
 		         System.out.print(c[i][l-1]+" ");
 	                }
 	         
-	                System.out.println();
+	                System.out.println(c[i][l-1]);
 
 }
 
