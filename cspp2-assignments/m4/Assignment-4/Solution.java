fb0d5abf081128ab7de67b115f8c41a15a8d8d78
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-31 14:36:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

---
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+public final class Solution {
+/**
+ * Constructs the object.
+ */
+    private Solution() {
+        //function.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param s { parameter_description }.
+     *
+     * @return { description_of_the_return_value }.
+     */
+    static String reverseString(final String s) {
+        /**
+         * { var_description }.
+         */
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+        return (rev);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+}
