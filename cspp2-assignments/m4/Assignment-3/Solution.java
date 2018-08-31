bc1c2e57e5414476feb6b726ac99163a61e1c630
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-31 14:48:53 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -18,7 +18,7 @@ public final class Solution {
      *
      * @return {description_of_the_return_value}.
      */
-    static String binaryToDecimal(final String s){
+    static String binaryToDecimal(final String s) {
         /**
          * {item_description}.
         */
@@ -35,14 +35,14 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args){
+    public static void main(final String[] args) {
         /**
          * {var_description}.
         */
         Scanner sc = new Scanner(System.in);
         String s;
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             s = sc.next();
             String res = binaryToDecimal(s);
             System.out.println(res);
