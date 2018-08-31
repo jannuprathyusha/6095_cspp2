dd2b7f350e56b415229001a0ef2553eb24bc7e5a
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-31 15:38:11 +0530

    conc

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Concatinate{
+	public static void main(String[]args){
+		Scanner s= new Scanner(System.in);
+		String s1=s.nextLine();
+		System.out.println(("Hello ").concat(s1).concat("!"));
+	}
+}
+		
+	    
