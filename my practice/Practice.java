3ca66e7b500fe3ae3419b13ddcaa09bf22b32af9
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-09-16 17:04:32 +0530

    finding max

--- my practice/Practice.java
+++ my practice/Practice.java
@@ -4,31 +4,18 @@ class Practice{
 		int i;
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		//int[] arr = new int[n];
-		int temp;
-		int sum1=0;
-		int sum2=0;
+		int[] arr = new int[n];
+		int min,max=0;
 		for (i=0;i<n;i++){
-			temp=s.nextInt();
-			if (temp%2==0){
-				sum1=sum1+temp;
+			arr[i]=s.nextInt();
+			if (arr[i]>max){
+				max = arr[i];
 			}
-			else {
-               sum2=sum2+temp;
+			
+
+
 			}
-
+			System.out.println(max);
 			
-			//System.out.println(arr[i]+" ");
-			
-		}
-		//for (i=0;i<n;i++){
-		System.out.println("Even numbers sum is"+sum1);
-		System.out.println("odd numbers sum is"+sum2);
-
-	//}
-		// int num = s.nextInt();
-		// num = num+1;
-		// System.out.println(num);
-
 	}
 }
