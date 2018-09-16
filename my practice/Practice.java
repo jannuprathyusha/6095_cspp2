938f9033390ad8df117b19e90e8ab003d9cec5cf
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-09-16 15:42:03 +0530

    printing an array

--- my practice/Practice.java
+++ my practice/Practice.java
@@ -2,15 +2,16 @@ import java.util.*;
 class Practice{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		//int[] arr = new	int[10];
-		// for (int i=0;i<10;i++){
-			//arr[]=
-			// System.out.println(i);
+		int[] arr = new	int[10];
+		for (int i=0;i<10;i++){
+			arr[i]=s.nextInt()+1;
+			//System.out.println();
 			
-		// }
-		int num = s.nextInt();
-		num = num+1;
-		System.out.println(num);
+		}
+		System.out.println(Arrays.toString(arr));
+		// int num = s.nextInt();
+		// num = num+1;
+		// System.out.println(num);
 
 	}
 }
