c67c449345d85b83fb3772f06abf436787366a1c
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-09-16 15:31:17 +0530

    scanning input from user

---
+++ my practice/Practice.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Practice{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		//int[] arr = new	int[10];
+		// for (int i=0;i<10;i++){
+			//arr[]=
+			// System.out.println(i);
+			
+		// }
+		int num = s.nextInt();
+		num = num+1;
+		System.out.println(num);
+
+	}
+}
