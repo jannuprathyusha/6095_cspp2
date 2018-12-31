855beb4a86999297c0529da33661c493f4e728f9
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-12-31 19:00:52 +0530

    "cspp2_m10_a1_01: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ cspp2-assignments/CSPP2 Remedial Exam/Solution.java
@@ -0,0 +1,108 @@
+import java.util.*;
+/**
+ * Class for main.
+ */
+class Hotel{
+	Reservation[] rooms;
+	// int size; 
+	Hotel(){
+		rooms = new Reservation[5];
+		size = 0;
+	}
+	 ​public int reserveRoom(String person) {
+	 	Reservation r = new Reservation(person);
+	 	for(int i = 0; i < rooms.length; i++) {
+	 		if(rooms[i] == null) {
+	 			rooms[i] = r;
+	 			size++;
+	 			return i+1;
+	 		}
+	 	}
+	 } 
+	 public boolean reserveRoom(String person, int roomNum)   {
+	 	if(rooms[roomNum] != null || size == rooms.length) {
+	 		return false;
+	 	} else {
+	 		Reservation r = new Reservation(person, roomNum);
+	 		rooms[roomNum] = r;
+	 		return true;
+	 	}
+	 }
+	  ​public void printReservations() {
+	  	for(int i = 0; i < rooms.length; i++) {
+	  		if(rooms[i] != null) {
+	  			System.out.println(rooms[i].getName() + " " + rooms[i].getRoom());
+	  		}
+	  	}
+	  }
+}
+public class Solution{
+
+  public static void main(String[] args){
+  		Hotel h = new Hotel();
+		String name;
+		int roomnum;
+  		Scanner scan = new Scanner(System.in);
+		int readLines = Integer.parseInt(scan.nextLine());
+		int c=0;
+		while (c<readLines) {
+			String line = scan.nextLine();
+			String tokens[]=line.split(" ");
+				switch(tokens[0])
+				{
+					case "reserve":
+									name = tokens[1];
+									roomnum = -1;
+								    //if we got a good name, try to reserve a room
+								    if(!((name==null) || (name.equals(""))))
+								        roomnum = h.reserveRoom(name);
+
+								    //give feedback
+								    if(roomnum!=-1)
+								        System.out.println(name+" "+roomnum);
+								 
+									break;
+
+					case "reserveN":
+									name = tokens[1];
+									//if we got a bad input, report failure
+
+								    if((name==null) || (name.equals("")))
+								        roomnum = -1;
+								    else{
+								     roomnum = Integer.parseInt(tokens[2]);
+								    }    
+								    //give feedback
+								    if(h.reserveRoom(name, roomnum))
+								        System.out.println(name+" "+roomnum);
+								    
+								    break;
+
+					case "print":
+								//print all the reservations done in the hotel
+								h.printReservations();
+								 break;
+					case "cancel":
+								//cancel the rooms for that particular person
+								name = tokens[1];
+								if(!((name==null) || (name.equals("")))){
+								        h.cancelReservations(name);
+								        System.out.println(name+" now has no reservations.");
+								    }else{
+								        System.out.println("No input");
+   								 }
+   								 break;
+					case "build":
+								//add more number of rooms to the existing rooms
+								roomnum = Integer.parseInt(tokens[1]);
+								
+							    if(h.buildRooms(roomnum))
+							        System.out.println("Added "+roomnum+" more rooms");
+							    else
+							        System.out.println("No rooms are added");
+							      break;
+				}
+				c++;
+		}
+	}
+}
