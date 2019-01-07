672e6fd4df76bc70d722c4abf47b9eff8a9eb947
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2019-01-07 21:46:18 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Poll/Question.java
+++ Poll/Question.java
@@ -3,15 +3,40 @@ import java.util.Arrays;
 class Question{
 	String question;
 	String[] options;
-	int[] 
+	int[] count; 
 	Question(){
        options = new String[4];
+       count = new int[4];
     }
     Question(String question,String[] options) {
-    	this.question = question;
-    	this.options = options;
+      this.question = question;
+      this.options = options;
     }
-    void setOptionVotes(){
-        
+    public String getText() {
+        return question;
+   }
+    
+   
+    public int indexOf(String str){
+      for(int i=0; i<4;i++){
+        if(options[i].equals(str)){
+          return i;
+        }
+      }
+      return 0;
+    }
+    public void setOptionVotes(int n){
+        count[n] = count[n] + 1;
+    }
+    public String commonSelectedOption(){
+        int max = 0;
+        int index = 0;
+        for(int i=0; i<options.length;i++){
+           if(count[i]>max){
+            max = count[i];
+            index=i;
+           }
+        }
+        return options[index];
     }
    }
--- Poll/Quiz.java
+++ Poll/Quiz.java
@@ -2,62 +2,19 @@ import java.util.Scanner;
 import java.util.Arrays;
 class Quiz {
 	Question[] questions;
-	Question[] options;
 	int size;
 	Quiz(){
       size = 0;
       questions = new Question[50];
 	}
-  public addQuestion(String question,String[] options) {
-       questions[size] = question;
+  public void addQuestion(Question q) {
+       questions[size] = q;
        ++size;
-       for(int i=0; i<options.length; i++){
-           options[i] = options;
-       }
   }
   public int size(){
   	return size;
   }
-  public Question getQuestion(int n){
+   public Question getQuestion(int n){
         return questions[n];
-    }
-   public String getText()	{
-        return String;
-   }
-   public String toString(){
-
-   }
-   public String commonSelectedOption(){
-    	int option0Response = 0;
-    	int option1Response = 0;
-    	int option2Response = 0;
-    	int option3Response = 0;
-    	for(int i=0; i<options.length;i++){
-    		if(p.response.equals(options[0])){
-             ++option0Response;
-    	}
-    	else if(p.response.equals(options[1])) {
-           ++option1Response;
-    	}
-    	else if(p.response.equals(options[2])){
-           ++option2Response;
-    	}
-    	else if(p.response.equals(options[3])){
-           ++option3Response;
-    	}
-    }
-    if(option0Response>option1Response>option2Response>option3Response){
-    	return options[0];
-    	}
-    else if(option1Response>option0Response>option2Response>option3Response){
-        return options[1];
-    }
-    else if(option2Response>option0Response>option2Response>option3Response){
-        return options[2];
-    }
-    else if(option3Response>option0Response>option2Response>option3Response){
-        return options[3];
-    }
-
     }
 }
